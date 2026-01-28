0214121f2ba074e60e38cb81a59a11fdec636d77
Bolilyi.VS <bolilyivs@gmail.com>
2026-01-29 00:42:15 +0500

    hw25-di: done

---
+++ hw25-di/homework-template/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+}
